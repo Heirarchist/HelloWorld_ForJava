d5cdf8a0da671e1b9b2c7632407946e30f71cd11
Heirarchist <ikenasser@gmail.com>
2014-10-25 22:47:52 -0400

    For Loops
    The basics of the for loop

---
+++ ForLoops.java
@@ -0,0 +1,13 @@
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i < 5; i = i + 1) {
+			//Part 1- code executes before loop starts, only once; Part 2- Condition for true/false; Part 3- Executed once after every loop
+			System.out.println("The value of i is: " + i );
+			
+		}
+
+	}
+
+}
